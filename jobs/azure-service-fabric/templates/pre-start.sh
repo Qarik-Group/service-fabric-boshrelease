43a60f8e46b08a058a1a559d14bc85d2016038a2
Dr Nic Williams <drnicwilliams@gmail.com>
2018-03-29 18:38:03 +1000

    include pre-built service-fabric debs; because it seems like a good idea at moment

--- jobs/azure-service-fabric/templates/pre-start.sh
+++ jobs/azure-service-fabric/templates/pre-start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-package=service-fabric-deps
+package=service-fabric-debs
 
 set -eu
 
 ls -alhS /var/vcap/packages/${package}/apt/cache/archives/*.deb
+
+echo "servicefabric servicefabric/accepted-eula-ga select true" | debconf-set-selections
+echo "servicefabricsdkcommon servicefabricsdkcommon/accepted-eula-ga select true" | debconf-set-selections
 
 debfiles=$(ls /var/vcap/packages/${package}/apt/cache/archives/*.deb)
 for debfile in ${debfiles[@]}; do
