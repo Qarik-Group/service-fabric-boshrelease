151693753f8e29fe4b0c18f16ea324d92b933023
Dr Nic Williams <drnicwilliams@gmail.com>
2018-03-29 21:33:59 +1000

    move to dev/test on AWS VMs as cgroup pkgs failing on garden containers

--- jobs/azure-service-fabric/templates/pre-start.sh
+++ jobs/azure-service-fabric/templates/pre-start.sh
@@ -2,14 +2,9 @@
 
 package=service-fabric-debs
 
-set -eu
-
 ls -alhS /var/vcap/packages/${package}/apt/cache/archives/*.deb
 
 echo "servicefabric servicefabric/accepted-eula-ga select true" | debconf-set-selections
 echo "servicefabricsdkcommon servicefabricsdkcommon/accepted-eula-ga select true" | debconf-set-selections
 
-debfiles=$(ls /var/vcap/packages/${package}/apt/cache/archives/*.deb)
-for debfile in ${debfiles[@]}; do
-  dpkg -x $debfile /
-done
+dpkg -i /var/vcap/packages/${package}/apt/cache/archives/*.deb
