629ec3963575a0308321ede14479f405cd628e40
Dr Nic Williams <drnicwilliams@gmail.com>
2018-04-23 19:46:21 -0700

    moving to xenial

--- jobs/azure-service-fabric/templates/pre-start.sh
+++ jobs/azure-service-fabric/templates/pre-start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-package=service-fabric-debs
-
-ls -alhS /var/vcap/packages/${package}/apt/cache/archives/*.deb
+ls -alhS /var/vcap/packages/service-fabric-debs/apt/cache/archives/*.deb
 
 echo "servicefabric servicefabric/accepted-eula-ga select true" | debconf-set-selections
 echo "servicefabricsdkcommon servicefabricsdkcommon/accepted-eula-ga select true" | debconf-set-selections
 
-dpkg -i /var/vcap/packages/${package}/apt/cache/archives/*.deb || { echo "Installation failed, ignore for now"; }
+dpkg -i /var/vcap/packages/service-fabric-debs/apt/cache/archives/python*.deb
+dpkg -i /var/vcap/packages/service-fabric-debs/apt/cache/archives/dotnet*.deb
+
+dpkg -i /var/vcap/packages/service-fabric-debs/apt/cache/archives/*.deb
